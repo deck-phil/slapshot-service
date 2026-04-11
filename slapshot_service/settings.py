63d3cf53b121c8c55ff37139d7107e18385873b5
deck-phil <philipdeck97@gmail.com>
2026-04-11 05:15:16 +0000

    Add login/logout routes, minimal login page, and navbar logout button

--- slapshot_service/settings.py
+++ slapshot_service/settings.py
@@ -132,4 +132,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = reverse_lazy("admin:login")
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/login/"
