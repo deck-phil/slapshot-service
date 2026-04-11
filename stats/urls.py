67f9b68cff89fdf7e9c1b99ae08f33b7e84953e9
deck-phil <philipdeck97@gmail.com>
2026-04-11 04:55:23 +0000

    Make season the homepage, rename Stats to All Time

--- stats/urls.py
+++ stats/urls.py
@@ -7,8 +7,8 @@ from stats.views.player_totals_view import player_totals_view
 from stats.views.season_stats_view import season_stats_view
 
 urlpatterns = [
-    path("", player_totals_view, name="player_totals"),
-    path("season/", season_stats_view, name="season_stats"),
+    path("", season_stats_view, name="season_stats"),
+    path("all-time/", player_totals_view, name="player_totals"),
     path("ingest/", ingest_trigger_view, name="ingest_trigger"),
     path("history/", match_history_view, name="match_history"),
     path("history/<int:slapshot_id>/", player_match_history_view, name="player_match_history"),
