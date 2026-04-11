34613b5f25d64674de78d8c751fcfd4220c3ea5b
deck-phil <philipdeck97@gmail.com>
2026-04-11 05:19:19 +0000

    Redirect 404s to homepage

--- slapshot_service/urls.py
+++ slapshot_service/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
+from stats.views.custom_404_view import custom_404_view
+
+handler404 = custom_404_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("login/", auth_views.LoginView.as_view(), name="login"),
