26268f792d0669caf9d656b6c8b59b39ed44afe9
Philip Deck <philipdeck97@gmail.com>
2026-04-11 15:30:15 +0000

    Revert "Redirect /ingest to /ingest/ before auth check intercepts"
    This reverts commit 9a390c5859aed243d63f248f9bc7981434150182.

--- stats/urls.py
+++ stats/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.generic import RedirectView
 
 from stats.views.ingest_trigger_view import ingest_trigger_view
 from stats.views.match_history_view import match_history_view
@@ -10,7 +9,6 @@ from stats.views.season_stats_view import season_stats_view
 urlpatterns = [
     path("", season_stats_view, name="season_stats"),
     path("all-time/", player_totals_view, name="player_totals"),
-    path("ingest", RedirectView.as_view(url="/ingest/", permanent=True)),
     path("ingest/", ingest_trigger_view, name="ingest_trigger"),
     path("history/", match_history_view, name="match_history"),
     path("history/<int:slapshot_id>/", player_match_history_view, name="player_match_history"),
